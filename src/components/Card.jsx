542ee6f3b0a5a41ca265b26ffffe37e0288e20a4
ilhye <144883820+ilhye@users.noreply.github.com>
2024-07-12 00:38:32 +0800

    update card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
-import "../styles/Card.css"
+import "../styles/Card.css";
 export default function Card() {
   return (
-    <div className="card intro-card">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-sm-8 left-card">
-            <h1>First Name Last Name</h1>
-            <h5>Description</h5>
-          </div>
-          <div className="col-sm-4">
-            <img src="src\assets\powerpuff-prof.png" alt="Powerpuff" className="intro-image" />
-          </div>
+    <>
+      <div className="card intro-card">
+        <div className="card-body">
+          <h1>First Name Last Name</h1>
+          <h5>Description</h5>
         </div>
       </div>
-    </div>
+      <img
+        src="src\assets\powerpuff-prof.png"
+        alt="Powerpuff"
+        className="intro-image"
+      />
+    </>
   );
 }
